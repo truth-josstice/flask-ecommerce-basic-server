9a548db62727877ae92e0fc177ad8c2c986e422b
Joss <joss.raine@gmail.com>
2025-07-17 19:53:33 +1000

    added a try except loop to return error code if values are not as expected, this is a VERY basic one, will create a more refined one once I have found it

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ class Product(db.Model):
         self.stock = stock
 
 
+
 # Create a class for ProductSchema
 class ProductSchema(SQLAlchemyAutoSchema):
     class Meta:
@@ -161,10 +162,11 @@ def delete_product(product_id):
 #UPDATE method PUT - updates all paramaters in the table, replaces any which have not been included, can create a new row if doesnt exist, PATCH - updates a single value in the table, cannot create new row in table
 @app.route("/products/<int:product_id>", methods=["PUT", "PATCH"])
 def update_product(product_id):
-    product = Product.query.get(product_id)
-
-    if product:
-        body_data = request.get_json()
+    stmt = db.select(Product).where(Product.id == product_id)
+    product = db.session.scalar(stmt)
+    try:
+        if product:
+            body_data = request.get_json()
         # update the values - SHORT CIRCUIT
         # product.name = body_data.get("name") or product.name
         # product.description = body_data.get("description") or product.description
@@ -172,17 +174,19 @@ def update_product(product_id):
         # product.stock = body_data.get("stock") or product.stock
 
         # get method can incorporate or implicitly (a little more DRY)
-        product.name = body_data.get("name", product.name)
-        product.description = body_data.get("description", product.description)
-        product.price = body_data.get("price", product.price)
-        product.stock = body_data.get("stock", product.stock)
+            product.name = body_data.get("name", product.name)
+            product.description = body_data.get("description", product.description)
+            product.price = body_data.get("price", product.price)
+            product.stock = body_data.get("stock", product.stock)
 
-        db.session.commit()
-        return jsonify(product_schema.dump(product))
+            db.session.commit()
+            return jsonify(product_schema.dump(product))
     
     # else:
-    else:
-        return jsonify({"message": f"Product with {product_id} does not exist"}), 404
+        else:
+            return jsonify({"message": f"Product with {product_id} does not exist"}), 404
+    except: 
+        return jsonify({"message": "request denied, invalid data type"}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_products.py
+++ tests/test_products.py
@@ -20,3 +20,56 @@ def test_get_products_success(client):
     assert response.status_code == 200
     data = response.get_json()
     assert isinstance(data, list)
+
+def test_update_product_success(client):
+    new_product = {
+        "name": "old product",
+        "description": "this product's name will be changed",
+        "price": 19.99
+    }
+
+    response = client.post('/products', json=new_product)
+    product = response.get_json()
+    product_id = product["id"]
+
+    #update the new product
+    update_data = {
+        "name": "new product value"
+    }
+
+    response = client.patch(f"/products/{product_id}", json=update_data)
+
+    assert response.status_code == 200
+    updated = response.get_json()
+    assert updated["name"] == "new product value"
+
+def test_update_nonexistant_product(client):
+    fake_id = 99999
+    update_data = {
+        "name": "ghost product",
+        "price": 49.99
+    }
+
+    response = client.patch(f"/products/{fake_id}", json=update_data)
+    assert response.status_code == 404
+    assert "does not exist" in response.get_json()["message"]
+
+def test_update_product_invalid_data(client):
+    product_data ={
+        "name": "Typo product",
+        "description": "Bad price",
+        "price": 5.0,
+        "stock": 2
+    }
+
+    response = client.post('/products', json=product_data)
+    product_id = response.get_json()["id"]
+
+    # set invalid price
+    updated_data = {
+        "price": "not a number"
+    }
+
+    response = client.patch(f"/products/{product_id}", json=updated_data)
+    assert response.status_code == 400
+    assert "denied" in response.get_json()["message"]
